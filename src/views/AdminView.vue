426ae1cb59fc8444c1e3489f5ff0cdf807ae8247
tang <sirisuke_t@silpakorn.edu>
2022-04-20 02:30:48 +0700

    Login And Home Page

---
+++ src/views/AdminView.vue
@@ -0,0 +1,40 @@
+<template>
+  <header>
+    <nav class="navbar fixed-top bg-success bg-gradient">
+      <div class="container-fluid">
+        <a class="navbar-brand">
+          <img
+            src="@/assets/logo_re.png"
+            alt=""
+            width="48"
+            height="64"
+            class="d-inline-block align-text-top"
+          />
+          <strong>โรงพยาบาลค่ายนวมินทราชินี</strong>
+        </a>
+        
+      </div>
+    </nav>
+
+    
+  </header>
+  <main>
+    <h1>Welcome</h1>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+h1 {
+  /* height: 200vh; */
+  padding: 7rem 0 0 0;
+  text-align: center;
+}
+
+a.navbar-brand {
+  color: white;
+}
+</style>
