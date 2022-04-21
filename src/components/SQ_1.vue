3116cf11e9cc01d35c8e543d25e81f5b1dff5477
JamZa <dotton123@hotmail.com>
2022-04-21 12:02:44 +0700

    eiei2

--- src/components/SQ_1.vue
+++ src/components/SQ_1.vue
@@ -1,32 +1,36 @@
 <template>
-  <div class="box">
-    <div class="doctorbox">
-      <h1>จำนวนผู้ป่วย</h1>
-      
-      <div class="line"></div>
-      <div class="subdocb">
-        <b>1000คน</b>
+  <div class="bigbox">
+    <div class="topbox">
+
+      <div class="doctorbox">
+        <h1>จำนวนผู้ป่วย</h1>
+        <div class="line"></div>
+        <div class="subdocb">
+          <b>1000คน</b>
+        </div>
+      </div>
+
+      <div class="patientbox">
+        <h1>จำนวนแพทย์</h1>
+        <div class="line"></div>
+        <div class="subpatb">
+          <b>20คน</b>
+        </div>
+      </div>
+
+      <div class="forwardbox">
+        <h1>จำนวนการส่งต่อ</h1>
+        <div class="line"></div>
+        <div class="subforb">
+          <b>200คน</b>
+        </div>
       </div>
     </div>
 
-    <div class="patientbox">
-      <h1>จำนวนแพทย์</h1>
-      <div class="line"></div>
-      <div class="subpatb">
-        <b>20คน</b>
+      <div class="botbox">
+        <div class="graphbox"></div>
       </div>
-    </div>
-
-    <div class="forwardbox">
-      <h1>จำนวนการส่งต่อ</h1>
-      <div class="line"></div>
-      <div class="subforb">
-        <b>200คน</b>
-      </div>
-    </div>
   </div>
-  <div class="grapgbox"></div>
-  <!-- <Sidebar /> -->
 </template>
 
 <script>
@@ -36,88 +40,102 @@ export default {
 </script>
 
 <style scoped>
-h1{
-  margin-top:10px ;
+h1 {
+  margin-top: 10px;
   text-align: center;
   font-size: 20px;
 }
-.box {
+.bigbox{
+  width: 100%;
+  height: 200vh;
+  display: flex;
+  flex-direction: column;
+  /* background-color: black; */
+}
+.topbox {
   margin: 0px;
   width: 100%;
-  height: 150px;
+  height: 200px;
   display: flex;
   justify-content: space-between;
 }
+.botbox {
+  margin-top: 50px;
+  width: 100%;
+  height: 600px;
+  display: flex;
+  justify-content: space-between;
+  /* background-color: blue; */
+}
 .doctorbox {
-  width: 350px;
+  width: 30%;
   height: 200px;
   display: flex;
   flex-direction: column;
   align-items: center;
   background-color: #c4c4c4;
-  border-radius:15px ;
+  border-radius: 15px;
 }
 .patientbox {
-  width: 350px;
+  width: 30%;
   height: 200px;
   display: flex;
   flex-direction: column;
   align-items: center;
   background-color: #c4c4c4;
-  border-radius:15px ;
+  border-radius: 15px;
 }
 .forwardbox {
-  width: 350px;
+  width: 30%;
   height: 200px;
   display: flex;
   flex-direction: column;
   align-items: center;
   background-color: #c4c4c4;
-  border-radius:15px ;
+  border-radius: 15px;
 }
-.subdocb{
+.subdocb {
   display: flex;
   justify-content: center;
   align-items: center;
   margin-top: 20px;
   width: 300px;
   height: 120px;
-  background-color: #E7E7E7;
-  border-radius:15px ;
+  background-color: #e7e7e7;
+  border-radius: 15px;
 }
-.subpatb{
+.subpatb {
   display: flex;
   justify-content: center;
   align-items: center;
   margin-top: 20px;
   width: 300px;
   height: 120px;
-  background-color: #E7E7E7;
-  border-radius:15px ;
+  background-color: #e7e7e7;
+  border-radius: 15px;
 }
-.subforb{
+.subforb {
   display: flex;
   justify-content: center;
   align-items: center;
   margin-top: 20px;
   width: 300px;
   height: 120px;
-  background-color: #E7E7E7;
-  border-radius:15px ;
+  background-color: #e7e7e7;
+  border-radius: 15px;
 }
-.line{
+.line {
   margin-top: 5px;
   width: 70%;
   height: 1.5px;
   background-color: black;
 }
-.graphbox
-{
-  margin: 0px;
+.graphbox {
+  margin-top: 0px;
   width: 100%;
-  height: 150px;
+  height: 600px;
   display: flex;
   justify-content: space-between;
+  background-color: #c4c4c4;
 }
-
 </style>
