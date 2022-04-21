b4a2858563463abbbe76d98d96588a3b61249ac9
tang <sirisuke_t@silpakorn.edu>
2022-04-21 11:08:49 +0700

    change to button

--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faUserSecret)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false
 
 const app = createApp(App);
 
