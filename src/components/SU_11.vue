6e81cc68ef8f7aa9752cc4e599ab57e5048121ed
tang <sirisuke_t@silpakorn.edu>
2022-05-04 11:58:29 +0700

    เพิ่มโรงพยาบาล

--- src/components/SU_11.vue
+++ src/components/SU_11.vue
@@ -11,7 +11,7 @@
     <div class="midbox">
       <div class="addbox">
         <button
-          @click="store.changePage(['เพิ่มโรงพยาบาล', 'ตั้งค่าโรงพยาบาล'])"
+          @click="store.changePage(['เพิ่มโรงพยาบาล', 'เพิ่มโรงพยาบาล'])"
           class="subadd"
         >
           เพิ่มโรงพยาบาล
