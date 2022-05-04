4c17f53ec80ba5e1fd2bb15a0ae8403e87130863
JamZa <dotton123@hotmail.com>
2022-05-04 13:47:14 +0700

    PatientResultView1+2

---
+++ src/views/PatientResultView2.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="main-admin">
+    <NavBar />
+    <div class="main-content">
+      <SideBar />
+      <div class="content-naw">
+        <TitleBar />
+        <VPTR_2/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PatientResultView2",
+};
+</script>
